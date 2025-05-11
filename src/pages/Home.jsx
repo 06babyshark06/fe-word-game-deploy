92e01190035cb515499917c6fbeb5ce397f2f094
Trần Đức An <110535543+06babyshark06@users.noreply.github.com>
2025-05-11 21:32:11 +0700

    add tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-export default function Home( { socket } ) {
+export default function Home({ socket }) {
   const navigate = useNavigate();
 
   const handlePlay = () => {
-    navigate('/lobby');
+    navigate("/lobby");
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>✏️ Welcome to Skribbl Clone</h1>
-      <button onClick={handlePlay} style={{ padding: '10px 20px', fontSize: '18px' }}>
+      <h1 className="text-3xl text-teal-500 font-bold underline">Hello world!</h1>
+      <button
+        onClick={handlePlay}
+        style={{ padding: "10px 20px", fontSize: "18px" }}
+      >
         ▶️ Play
       </button>
     </div>
